e1d85844a8222300a62a21c2c02fb22dc0b4804d
BIFIQ <muhammadbumi.sfq@gmail.com>
2026-03-05 20:59:36 +0700

    Disabled CardSystem.cs and Card.cs due to error
    Tutorial video did not include explanation or code of "Minion" class and the name "Card" was already used in the previous vid. Some comments also complain the same thing. Not sure how to fix.

--- Assets/Battle Assets/Battle_scripts/General/ActionReaction/Other/Card.cs
+++ Assets/Battle Assets/Battle_scripts/General/ActionReaction/Other/Card.cs
@@ -1,12 +1,13 @@
-using UnityEngine;
+//code is currently disabled because errors in the tutorial video that I can't find the fix for. Ep. 5's offlink video jump around without showing the full script
+// using UnityEngine;
 
-public class Card : MonoBehaviour
-{
-    void OnMouseDown()
-    {
-        if (ActionSystem.Instance.IsPerforming) return;
-        DrawCardGA drawCardGA = new();
-        ActionSystem.Instance.Perform(drawCardGA);
-        Destroy(gameObject);
-    }
-}
+// public class CardReaction : MonoBehaviour //in the video CardReaction was called Card, but that caused erorrs because of the existing Card class, so I renamed it to CardReaction. will change back to Card when the existing Card class is removed.
+// {
+//     void OnMouseDown()
+//     {
+//         if (ActionSystem.Instance.IsPerforming) return;
+//         DrawCardGA drawCardGA = new();
+//         ActionSystem.Instance.Perform(drawCardGA);
+//         Destroy(gameObject);
+//     }
+// }
--- Assets/Battle Assets/Battle_scripts/General/ActionReaction/Other/CardSystem.cs
+++ Assets/Battle Assets/Battle_scripts/General/ActionReaction/Other/CardSystem.cs
@@ -1,16 +1,30 @@
-using UnityEngine;
+//code is currently disabled because errors in the tutorial video that I can't find the fix for. Ep. 5's offlink video jump around without showing the full script
 
-public class CardSystem : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+// using DG.Tweening;
+// using UnityEngine;
+// using System.Collections;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+
+// public class CardSystem : MonoBehaviour
+// {
+//     [SerializeField] private Card cardPrefab;
+//     [SerializeField] private Transform spawn;
+//     [SerializeField] private Transform hand;
+
+//     private void OnEnable()
+//     {
+//         ActionSystem.AttachPerformer<DrawCardGA>(DrawCardPerformer);
+//     }
+
+//     private void OnDisable()
+//     {
+//         ActionSystem.DetachPerformer<DrawCardGA>();
+//     }
+
+//     private IEnumerator DrawCardPerformer(DrawCardGA drawCardGA)
+//     {
+//         CardReaction card = Instantiate(cardPrefab, spawn.position, Quaternion.identity);
+//         Tween tween = card.transform.DOMove(hand.position, 0.5f);
+//         yield return tween.WaitForCompletion();
+//     }
+// }
