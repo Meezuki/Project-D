01ba67520f3944a5dcbfeb6030f259b4d20cb9ee
Marco <marcoodenleo25@gmail.com>
2026-05-08 16:17:45 +0700

    Burn Effect & Burn System

---
+++ Assets/Battle Assets/Battle_scripts/GameActions/ApplyBurnGA.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ApplyBurnGA : GameAction
+{
+    public int BurnDamage { get; private set; }
+
+    public CombatantView Target { get; private set; }
+
+    public ApplyBurnGA(int burnDamage, CombatantView target)
+    {
+        BurnDamage = burnDamage;
+        Target = target;
+    }
+
+}
---
+++ Assets/Battle Assets/Battle_scripts/Systems/BurnSystem.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class BurnSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject burnVFX;
+
+    private void OnEnable()
+    {
+        ActionSystem.AttachPerformer<ApplyBurnGA>(ApplyBurnPerformer);
+
+        
+    }
+
+    private void OnDisable()
+    {
+        ActionSystem.DetachPerformer<ApplyBurnGA>();
+    }
+
+    private IEnumerator ApplyBurnPerformer(ApplyBurnGA applyBurnGA)
+    {
+        CombatantView target = applyBurnGA.Target;
+        Instantiate(burnVFX, target.transform.position, Quaternion.identity);
+        target.Damage(applyBurnGA.BurnDamage);
+        target.RemoveStatusEffect(StatusEffectType.BURN, 1);
+        yield return new WaitForSeconds(1f);  
+    }
+}
